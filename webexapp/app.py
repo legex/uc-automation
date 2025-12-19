66ef84674e55a5a56429cf2cf7f7d9ef4195be3b
Abhishek Shukla <abshukla@akamai.com>
2025-12-19 17:03:10 +0530

    Created some more routes

--- webexapp/app.py
+++ webexapp/app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Request, UploadFile, HTTPException, status
 from fastapi.responses import FileResponse
 from ldapapi.updateldap import update_contacts_num, update_contacts_num_withDID
-from webexapp.main import batch_update_ldap
+from webexapp.main import batch_routepattern_auto, batch_update_ldap, batch_update_webex_gen, batch_update_webex_acd
 from cucmapi.axlop import AXLOperations
 from cucmapi.axlroutepattern import AXLRoutePatternOperations
 from metadata.settings import extension_prefix
@@ -16,6 +16,7 @@ from webexapi.webexoperations import WebexOperation
 from webexapi.webexnumberadd import addnumber
 from webexapp.webexbotbase import WebexbotBase
 from utils.logger import setup_logger
+# Load environment variables from .env file
 load_dotenv()
 API_TOKEN = os.getenv("WEBEXBOTTOKEN")
 
@@ -33,6 +34,13 @@ class QueryModelLdap(BaseModel):
     extension: str
     externalnumber: str | None = None
 
+class QueryModelWebex(BaseModel):
+    username: str
+    extension: str
+    region: str
+    externalnumber: str | None = None
+
+
 webexbot = WebexbotBase()
 app = FastAPI()
 orgId = ""
@@ -46,6 +54,24 @@ async def room_webhook(request: Request):
     if room_id:
         webexbot.send_message(room_id, "Hello! Welcome to the Webex Room.")
     return {"status": "success"}
+
+
+@app.get("/download/template/{template_name}")
+async def download_template(template_name: str):
+    templates = {
+        "ldap_update": "templates/ldap_update_template.csv",
+        "webex_general_update": "templates/webex_general_update_template.csv",
+        "webex_acd_update": "templates/webex_acd_update_template.csv",
+        "number_add": "templates/number_add_template.csv",
+        "cucm_route_pattern": "templates/cucm_route_pattern_template.csv"
+    }
+    file_path = templates.get(template_name)
+    if not file_path or not os.path.exists(file_path):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Template not found.")
+    return FileResponse(path=file_path,
+                        filename=os.path.basename(file_path),
+                        media_type='application/octet-stream')
 
 @app.post("/numberadd")
 async def number_add(file: UploadFile):
@@ -68,7 +94,7 @@ async def number_add(file: UploadFile):
     except HTTPException as e:
         return {"error": str(e)}
 
-@app.get("/download/numberadd/{filename}")
+@app.get("/download/{filename}")
 async def download_numberadd_response(filename: str):
     file_path = f"resultfiles/numberadd_response_{filename}"
     if not os.path.exists(file_path):
@@ -120,3 +146,109 @@ async def update_ldap_numbers(query: QueryModelLdap | None = None,
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                                 detail=f"Error processing file: {str(e)}") from e
+
+@app.post("/updatewebexgeneral")
+async def update_webex_general(query: QueryModelWebex | None = None,
+                                 file: UploadFile | None = None):
+    if query is None and file is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Either query or file must be provided.")
+    if query:
+        if query.externalnumber:
+            try:
+                webex_results = webex_acd_mig.patch_dn_acd(
+                    query.username,
+                    query.externalnumber,
+                    query.extension,
+                    query.region
+                    )
+                return {"Status": "Webex update initiated", "Detail": webex_results}
+            except Exception as e:
+                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                    detail=f"Error updating Webex with DID: {str(e)}") from e
+        try:
+            webex_results = webex_mig_gen.patch_license_dn(
+                query.username,
+                query.extension,
+                query.region
+                )
+            return {"Status": "Webex update initiated", "Detail": webex_results}
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail=f"Error updating Webex: {str(e)}") from e
+    if file:
+        if file.content_type != 'text/csv':
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Invalid file type. Please upload a CSV file.")
+        contents = await file.read()
+        if not contents:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Uploaded file is empty")
+        try:
+            response = batch_update_webex_gen(pd.io.common.BytesIO(contents), file.filename)
+            return {"Status": "Success", "Detail": response}
+        except HTTPException as e:
+            return {"error": str(e)}
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail=f"Error processing file: {str(e)}") from e
+
+@app.post("/updatewebexacd")
+async def update_webex_acd(query: QueryModelWebex | None = None,
+                                 file: UploadFile | None = None):
+    if query is None and file is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Either query or file must be provided.")
+    if query:
+        if query.externalnumber:
+            try:
+                webex_results = webex_acd_mig.patch_dn_acd(
+                    query.username,
+                    query.externalnumber,
+                    query.extension,
+                    query.region
+                    )
+                return {"Status": "Webex ACD update initiated", "Detail": webex_results}
+            except Exception as e:
+                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                    detail=f"Error updating Webex ACD with DID: {str(e)}") from e
+        else:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="External number must be provided for ACD updates.")
+    if file:
+        if file.content_type != 'text/csv':
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Invalid file type. Please upload a CSV file.")
+        contents = await file.read()
+        if not contents:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Uploaded file is empty")
+        try:
+            response = batch_update_webex_acd(pd.io.common.BytesIO(contents), file.filename)
+            return {"Status": "Success", "Detail": response}
+        except HTTPException as e:
+            return {"error": str(e)}
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail=f"Error processing file: {str(e)}") from e
+
+@app.post("/routepattern")
+async def create_route_pattern(file: UploadFile):
+    if file.content_type != 'text/csv':
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Invalid file type. Please upload a CSV file.")
+    contents = await file.read()
+    if not contents:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Uploaded file is empty")
+    try:
+        response = batch_routepattern_auto(
+            pd.io.common.BytesIO(contents),
+            file.filename
+            )
+        return {"Status": "Success", "Detail": response}
+    except HTTPException as e:
+        return {"error": str(e)}
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Error processing file: {str(e)}") from e
--- webexapp/main.py
+++ webexapp/main.py
@@ -1,7 +1,10 @@
 import os
-import sys
 import pandas as pd
-from ldapapi.updateldap import update_contacts_num, update_contacts_num_withDID
+from ldapapi.updateldap import (
+    update_contacts_num,
+    update_contacts_num_withDID,
+    update_general_contacts_num_withDID
+)
 from cucmapi.axlop import AXLOperations
 from cucmapi.axlroutepattern import AXLRoutePatternOperations
 from metadata.settings import extension_prefix, exclude_list
@@ -13,7 +16,15 @@ from utils.logger import setup_logger
 logger = setup_logger('webmainapp', 'log/webmainapp.log')
 
 prefixes = extension_prefix
-exclude_list = exclude_list
+excluded_list = exclude_list
+axloperations = AXLOperations()
+axlrp = AXLRoutePatternOperations()
+webex_mig_gen = WebexGenMigration()
+webex_acd_mig = WebexMigACD()
+currentdir = os.getcwd()
+webop = WebexOperation()
+
+
 def batch_update_ldap(file, filename):
     """Update CUCM Extension"""
     df = pd.read_csv(file, dtype={'targetNum': str})
@@ -22,19 +33,21 @@ def batch_update_ldap(file, filename):
         username = row['UserId']
         extension = row['targetNum']
         externalnumber = row.get('ExternalNumber', None)
-        if username not in exclude_list:
+        if username not in excluded_list:
             if externalnumber:
                 try:
-                    ad_result = update_contacts_num_withDID(username, extension, externalnumber)
+                    ad_result = update_contacts_num_withDID(
+                        username, extension, externalnumber)
                     status_on_ad = "Success" if ad_result else "Failed"
-                except Exception as e:
-                    logger.error("Error updating AD with DID for %s: %s", username, e)
+                except (ConnectionError, ValueError, AttributeError) as e:
+                    logger.error(
+                        "Error updating AD with DID for %s: %s", username, e)
                     status_on_ad = "Error"
             else:
                 try:
                     ad_result = update_contacts_num(username, extension)
                     status_on_ad = "Success" if ad_result else "Failed"
-                except Exception as e:
+                except (ConnectionError, ValueError, AttributeError) as e:
                     logger.error("Error updating AD for %s: %s", username, e)
                     status_on_ad = "Error"
         else:
@@ -45,5 +58,178 @@ def batch_update_ldap(file, filename):
             "extension": extension,
             "status_on_ad": status_on_ad
         }
-        pd.DataFrame([ad_result]).to_csv(f"resultfiles/ldap_response_{filename}", mode='a', header=False, index=False)
+        pd.DataFrame(
+            [ad_result]).to_csv(f"resultfiles/ldap_response_{filename}",
+                                mode='a',
+                                header=False,
+                                index=False
+                                )
+    return "Script Run is Finished"
+
+
+def batch_update_webex_gen(file, filename):
+    """Update Webex Extension"""
+    df = pd.read_csv(file, dtype={'extension': str, 'ContactNumber': str})
+    for _, row in df.iterrows():
+        username = row['UserId']
+        phonenumber = row.get('ContactNumber', None)
+        extension = row['extension']
+        region = row['Country']
+        ad_num = prefixes[region]+row['extension']
+        email = row["Email"]
+        status_on_ad = ""
+        status_on_webex = ""
+        print(extension)
+        if username not in excluded_list:
+            if phonenumber:
+                try:
+                    webex_results = webex_acd_mig.patch_dn_acd(
+                        email,
+                        phonenumber,
+                        extension,
+                        region
+                    )
+                    status_on_webex = "Success" if webex_results else "Failed"
+                except (ConnectionError, ValueError, AttributeError) as e:
+                    logger.error(
+                        "Error updating Webex with DID for %s: %s", username, e)
+                    status_on_webex = "Error"
+                if status_on_webex == "Success":
+                    try:
+                        ad_result = update_general_contacts_num_withDID(
+                            username,
+                            phonenumber,
+                            ad_num
+                        )
+                        status_on_ad = "Success" if ad_result else "Failed"
+                    except (ConnectionError, ValueError, AttributeError) as e:
+                        logger.error(
+                            "Error updating AD for %s: %s", username, e)
+                        status_on_ad = "Error"
+            try:
+                webex_results = webex_mig_gen.patch_license_dn(
+                    email,
+                    extension,
+                    region
+                )
+                status_on_webex = "Success" if webex_results else "Failed"
+            except (ConnectionError, ValueError, AttributeError) as e:
+                logger.error("Error updating Webex for %s: %s", username, e)
+                status_on_webex = "Error"
+            if status_on_webex == "Success":
+                try:
+                    ad_result = update_contacts_num(
+                        username,
+                        ad_num
+                    )
+                    status_on_ad = "Success" if ad_result else "Failed"
+                except (ConnectionError, ValueError, AttributeError) as e:
+                    logger.error("Error updating AD for %s: %s", username, e)
+                    status_on_ad = "Error"
+            else:
+                status_on_ad = "Skipped"
+        else:
+            logger.info("User %s is in Exclude list", username)
+            status_on_ad = "Skipped"
+
+        webex_mig_result = {
+            "username": username,
+            "email": email,
+            "FullExtension": ad_num,
+            "phoneNum": phonenumber,
+            "extension": extension,
+            "region": region,
+            "status_on_cucm": status_on_webex,
+            "status_on_ad": status_on_ad
+        }
+        pd.DataFrame(
+            [webex_mig_result]).to_csv(f"resultfiles/webex_migresult_{filename}",
+                                       mode='a',
+                                       header=False,
+                                       index=False
+                                       )
+    return "Script Run is Finished"
+
+def batch_update_webex_acd(file, filename):
+    """Update Webex Extension"""
+    df = pd.read_excel(file, dtype={'extension': str, 'ContactNumber': str})
+    for _, row in df.iterrows():
+        username = row['UserId']
+        phonenumber = row['ContactNumber']
+        extension = row['extension']
+        region = row['Country']
+        ad_num = prefixes[region]+row['extension']
+        email = row["Email"]
+        print(extension)
+
+        try:
+            webex_results = webex_acd_mig.patch_dn_acd(
+                email,
+                phonenumber,
+                extension,
+                region
+            )
+            status_on_webex = "Success" if webex_results else "Failed"
+        except (ConnectionError, ValueError, AttributeError) as e:
+            logger.error("Error updating CUCM for %s: %s", username, e)
+            status_on_webex = "Error"
+        if status_on_webex == "Success":
+            try:
+                ad_result = update_contacts_num_withDID(
+                    username,
+                    phonenumber,
+                    ad_num
+                )
+                status_on_ad = "Success" if ad_result else "Failed"
+            except (ConnectionError, ValueError, AttributeError) as e:
+                logger.error("Error updating AD for %s: %s", username, e)
+                status_on_ad = "Error"
+        else:
+            status_on_ad = "Skipped"
+
+        acd_result = {
+            "username": username,
+            "email": email,
+            "phoneNum": phonenumber,
+            "extension": extension,
+            "region": region,
+            "status_on_cucm": status_on_webex,
+            "status_on_ad": status_on_ad
+        }
+        pd.DataFrame([acd_result]).to_csv(
+            f"resultfiles/acd_migresult_{filename}",
+            mode='a',
+            header=False,
+            index=False
+            )
+    return "Script Run is Finished"
+
+
+def batch_routepattern_auto(file, filename):
+    """Create Route Pattern in CUCM from CSV"""
+    df = pd.read_csv(file, dtype={'ContactNumber': str})
+    for _, row in df.iterrows():
+        routepattern = f"{row['ContactNumber']}"
+        username = row["UserId"]
+        try:
+            update_partition = axlrp.update_line(routepattern)
+            update_rp = axlrp.create_routepattern(routepattern, username)
+            status_on_cucm = "Success" if update_rp else "Failed"
+            status_partition = "Success" if update_partition else "Failed"
+        except (ConnectionError, ValueError, AttributeError) as e:
+            logger.error(
+                "Error updating CUCM for route pattern %s: %s", routepattern, e)
+            status_on_cucm = "Error"
+            status_partition = "Error"
+        results = {
+            "routepattern": routepattern,
+            "status_on_cucm": status_on_cucm,
+            "rp_update_status": status_partition,
+        }
+        pd.DataFrame([results]).to_csv(
+            f"resultfiles/rpupdateresult_{filename}",
+            mode='a',
+            header=False,
+            index=False
+            )
     return "Script Run is Finished"
