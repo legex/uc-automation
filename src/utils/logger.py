49c3e6290de330bfbcd8d70154a5a03cdc222fdf
Abhishek Shukla <abshukla@akamai.com>
2026-02-04 13:00:08 +0530

    update with UI

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -7,7 +7,7 @@ structure creation.
 """
 import logging
 import os
-
+ENVIRONMENT_LOCAL = False
 def setup_logger(name: str, log_file: str, level=logging.INFO) -> logging.Logger:
     """
     Configure and return a logger instance with file output.
@@ -34,11 +34,14 @@ def setup_logger(name: str, log_file: str, level=logging.INFO) -> logging.Logger
         File encoding: UTF-8
         File mode: Append ('a')
     """
-    os.makedirs(os.path.dirname(f"tmp/{log_file}"), exist_ok=True)
+    if ENVIRONMENT_LOCAL:
+        log_file = f"tmp/{log_file}"
+
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    handler = logging.FileHandler(f"tmp/{log_file}", mode='a', encoding='utf-8')
+    handler = logging.FileHandler(log_file, mode='a', encoding='utf-8')
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
